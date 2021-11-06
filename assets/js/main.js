57b43f2c1976b66ad090768badaf1076f385d0fe
Antônio Cosmo <antoniocosmosilvaneto@gmail.com>
2021-11-06 16:54:01 -0300

    salvando as tarefas no localstore

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,35 @@
 
 (function(){
-	const btnNew = document.querySelector('.btn-new')
-	const input = document.querySelector('.form-in')
-	const list = document.querySelector('.list-task')
 
-	const createLi = () => {
-	  const li = document.createElement('li')
-	  li.classList.add('task')
-	  return li
-	}
+	const input = document.querySelector('.form-in')
+	const listTasks = document.querySelector('.list-task')
+
 	const btnDelete = () => {
 	  const btn = document.createElement('button')
 	  btn.classList.add('delete')
 	  btn.innerText = 'deletar'
 	  return btn
 	}
+
+	const createLi = (valor) => {
+		const li = document.createElement('li')
+		li.classList.add('task')
+		li.innerHTML = `<p>${valor}</>`
+		const btn = btnDelete()
+		li.appendChild(btn)
+	  listTasks.appendChild(li)
+	}
+
 	const clearInput = () => {
 	  input.value = ''
 	  input.focus()
 	}
-	const createTask = e => {
+
+	const createTask = (e) => {
 	  e.preventDefault()
-	  const valor = input.value
-	  const li = createLi()
-	  const btn = btnDelete()
-	  li.innerHTML = `<p>${valor}</>`
-	  li.appendChild(btn)
-	  list.appendChild(li)
+		createLi(input.value)
 	  clearInput()
+		saveTask()
 	}
 
 	document.addEventListener('click', e => {
@@ -36,11 +38,38 @@
 	    e.preventDefault()
 	    input.focus()
 	  } else if (el.classList.contains('btn-new')) {
-	    createTask(e)
+	    createTask(e,input.value)
 	  }
 
 	  if (el.classList.contains('delete')) {
 	    el.parentElement.remove()
+			saveTask()
 	  }
 	})
+
+	const saveTask = () => {
+		const liTask = listTasks.querySelectorAll('.task');
+		const listOfTask = [];
+	
+		for (let task of liTask) {
+			let taskText = task.innerText;
+			taskText = taskText.replace('deletar', '').trim();
+			listOfTask.push(taskText);
+		}
+	
+		const taskJSON = JSON.stringify(listOfTask);
+		localStorage.setItem('task', taskJSON);
+	}
+
+	const addTaskSave = () => {
+		const tasks = localStorage.getItem('task');
+		const listOfTask = JSON.parse(tasks);
+	
+		for(let task of listOfTask) {
+			createLi(task);
+		}
+	}
+
+	addTaskSave()
+
 })()
